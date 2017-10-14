3ff9df5ecb0ca94742adc7506377893fd6662022
Pead Nong <anuchit.prasertsang@gmail.com>
2017-10-14 10:21:38 +0700

    fix data access level

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import db from '@/firebase.conf'
 
-const getShopById = async (id) => await db.ref('shop_id').child(id).once('value')
+const getShopById = async (id) => await db.child('shop_id').child(id).once('value')
 
 export default {
   getShopById
