40a8e6b4bb9032c7ea016ba675199776c5171d79
Pead Nong <anuchit.prasertsang@gmail.com>
2017-10-15 23:54:23 +0700

    work around storageBucket undefind becase it is '' and process.conf have no in dev env

--- src/firebase.conf.js
+++ src/firebase.conf.js
@@ -3,7 +3,7 @@ const apiKey = process.env.apiKey || process.conf.apiKey
 const authDomain = process.env.authDomain || process.conf.authDomain
 const databaseURL = process.env.databaseURL || process.conf.databaseURL
 const projectId = process.env.projectId || process.conf.projectId
-const storageBucket = process.env.storageBucket || process.conf.storageBucket
+const storageBucket = ''
 const messagingSenderId = process.env.messagingSenderId || process.conf.messagingSenderId
 
 var firebaseApp = Firebase.initializeApp({
