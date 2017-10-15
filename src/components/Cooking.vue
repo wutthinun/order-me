9c52ee90d92babdd966047472e300e40cac5e85b
Nhong Anuchit Prasertsang <anuchitp@thoughtworks.com>
2017-10-15 17:25:29 +0700

    add order queueup amount

--- src/components/Cooking.vue
+++ src/components/Cooking.vue
@@ -1,34 +1,47 @@
 <template>
-  <div class="container">
-    <div class="box" v-for="(items, key1) in orders" :key="key1">
+  <div class="container is-centered">
+    <section>
       <div class="columns">
-        <div class="column is-12">
-          <label class="">Desk: {{ items.desk }}</label>
+        <div class="column">
+          <div class="tags has-addons">
+            <span class="tag">รายการอาหารรอคิว</span>
+            <span class="tag is-warning">0</span>
+          </div>
         </div>
-        <div class="column is-hidden-mobile"></div>
-        <div class="column is-hidden-mobile"></div>
-        <div class="column is-hidden-mobile"></div>
       </div>
-      <table class="table is-fullwidth">
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Amount</th>
-            <!-- <th></th> -->
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, key2) in items.order" :key="key2">
-            <td>{{ item.name }}</td>
-            <td>{{ item.amount }}</td>
-            <!-- <td><button class="button is-small is-info" @click="increment(order)">done</button></td> -->
-          </tr>
-        </tbody>
-      </table>
-      <div class="column">
-        <button class="button is-success is-fullwidth" @click="done(key1)">Done</button>
+    </section>
+    <section>
+      <div class="box" v-for="(items, key1) in orders" :key="key1">
+        <div class="columns">
+          <div class="column is-12">
+            <label class="">Desk: {{ items.desk }}</label>
+          </div>
+          <div class="column is-hidden-mobile"></div>
+          <div class="column is-hidden-mobile"></div>
+          <div class="column is-hidden-mobile"></div>
+        </div>
+        <table class="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Amount</th>
+              <!-- <th></th> -->
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, key2) in items.order" :key="key2">
+              <td>{{ item.name }}</td>
+              <td>{{ item.amount }}</td>
+              <!-- <td><button class="button is-small is-info" @click="increment(order)">done</button></td> -->
+            </tr>
+          </tbody>
+        </table>
+        <div class="column">
+          <button class="button is-success is-fullwidth" @click="done(key1)">Done</button>
+        </div>
       </div>
-    </div>
+    </section>
+
   </div>
 </template>
 <script>
