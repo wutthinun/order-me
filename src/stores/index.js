7537235b97e305e9c003b3f887228405c23607eb
wutthinun <godofcyber666@gmail.com>
2017-10-20 16:19:17 +0700

    Split mutations to file

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,8 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 import * as actions from './actions'
-import * as types from './mutation-types'
 import * as getters from './getters'
-import _ from 'lodash'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
@@ -24,40 +23,6 @@ const state = {
 export default new Vuex.Store({
   actions,
   state,
-  mutations: {
-    [types.ADD_TO_CART] (state, { item }) {
-      const order = _.find(state.cart.orders, (o) => o.id === item.id)
-      if (order) {
-        order.amount++
-      } else {
-        item.status = 'SELECT'
-        item.amount = 1
-        state.cart.orders.push(item)
-      }
-    },
-    [types.CLEAR_SELECT_CART] (state) {
-      state.cart.orders.splice(0)
-    },
-    [types.GET_ORDERED] (state, ordered) {
-      state.cart.ordered = ordered
-    },
-    [types.GET_ALL_ITEMS] (state, db) {
-      db.child('items').on('value', snapshot => {
-        state.items = _.map(snapshot.val(), (item, key) => {
-          item.id = key
-          return item
-        })
-      })
-    },
-    addOrder (state, key) {
-      if (state.orderList[key]) {
-        state.orderList[key].amount += 1
-      } else {
-        Vue.set(state.orderList, key, state.order)
-      }
-      state.total += state.order.price
-      state.amount += 1
-    }
-  },
+  mutations,
   getters
 })
---
+++ src/stores/mutations.js
@@ -0,0 +1,43 @@
+import * as types from './mutation-types'
+import Vue from 'vue'
+import _ from 'lodash'
+
+export default {
+  [types.ADD_TO_CART] (state, { item }) {
+    const order = _.find(state.cart.orders, (o) => o.id === item.id)
+    if (order) {
+      order.amount++
+    } else {
+      item.status = 'SELECT'
+      item.amount = 1
+      state.cart.orders.push(item)
+    }
+  },
+
+  [types.CLEAR_SELECT_CART] (state) {
+    state.cart.orders.splice(0)
+  },
+  
+  [types.GET_ORDERED] (state, ordered) {
+    state.cart.ordered = ordered
+  },
+
+  [types.GET_ALL_ITEMS] (state, db) {
+    db.child('items').on('value', snapshot => {
+      state.items = _.map(snapshot.val(), (item, key) => {
+        item.id = key
+        return item
+      })
+    })
+  },
+
+  addOrder (state, key) {
+    if (state.orderList[key]) {
+      state.orderList[key].amount += 1
+    } else {
+      Vue.set(state.orderList, key, state.order)
+    }
+    state.total += state.order.price
+    state.amount += 1
+  }
+}
