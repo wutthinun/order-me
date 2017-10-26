998957e89e245fcaed5395dc406177877465403c
wutthinun <godofcyber666@gmail.com>
2017-10-26 16:16:12 +0700

    Edit mutation types

--- src/stores/mutation-types.js
+++ src/stores/mutation-types.js
@@ -2,8 +2,5 @@ export const ADD_TO_CART = 'ADD_TO_CART'
 export const CLEAR_SELECT_CART = 'CLEAR_SELECT_CART'
 export const GET_ORDERED = 'GET_ORDERED'
 export const GET_ALL_ITEMS = 'GET_ALL_ITEMS'
-<<<<<<< HEAD
 export const GET_WHOLE_ORDERS = 'GET_WHOLE_ORDERS'
-=======
 export const PURCHASE = 'PURCHASE'
->>>>>>> Implement purchase function
