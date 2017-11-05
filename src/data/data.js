6a4a56db9e6a3cfe23d393292f773854b3236fd4
Nhong Anuchit Prasertsang <anuchitp@thoughtworks.com>
2017-11-05 15:52:26 +0700

    refactor remove duplication

--- src/data/data.js
+++ src/data/data.js
@@ -1,32 +1,34 @@
 import db from '@/firebase.conf'
 
+const shop = db.child(localStorage.getItem('shop_id'))
+
 const getShopById = async id => await db.child('shop_id').child(id).once('value')
 
-const getOrdered = async (orderKey) => await db.child(localStorage.getItem('shop_id')).child('orders').child(orderKey).once('value')
+const getOrdered = async (orderKey) => await shop.child('orders').child(orderKey).once('value')
 
 const getWholeOrders = async () => {
   let wholeOrders = []
-  await db.child(localStorage.getItem('shop_id')).child('orders').on('value', snapshot => {
+  await shop.child('orders').on('value', snapshot => {
     wholeOrders = snapshot.val()
   })
   return wholeOrders
 }
 
 const initOrder = async () => {
-  return await db.child(localStorage.getItem('shop_id')).child('orders').push({
+  return await shop.child('orders').push({
     desk: localStorage.getItem('desk'),
     time: new Date().toString(),
     status: 'UNPAID'
   }).key
 }
 
-const getUnpaidOrder = async () => await db.child(localStorage.getItem('shop_id')).child('orders').orderByChild('status').equalTo('UNPAID').once('value')
+const getUnpaidOrder = async () => await shop.child('orders').orderByChild('status').equalTo('UNPAID').once('value')
 
-const purchase = async (orderKey) => await db.child(localStorage.getItem('shop_id')).child('orders').child(orderKey).update({ status: 'PAID' })
+const purchase = async (orderKey) => await shop.child('orders').child(orderKey).update({ status: 'PAID' })
 
 const saveOrder = async (order, orderkey) => {
   if (orderkey) {
-    db.child(localStorage.getItem('shop_id')).child('orders').child(orderkey).update(order, (error) => {
+    shop.child('orders').child(orderkey).update(order, (error) => {
       if (error) {
         console.error('ERROR: ', error)
       } else {
@@ -34,7 +36,7 @@ const saveOrder = async (order, orderkey) => {
       }
     })
   } else {
-    orderkey = db.child(localStorage.getItem('shop_id')).child('orders').push(order).key
+    orderkey = shop.child('orders').push(order).key
   }
   return orderkey
 }
